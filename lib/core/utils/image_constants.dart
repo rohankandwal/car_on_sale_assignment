e0267d2d483e7a92a5dfec4b008272db30ffa8b9
Rohan Kandwal <rohankandwal@yahoo.in>
2025-03-29 20:25:35 +0100

    Pre-Commit hook added

---
+++ lib/core/utils/image_constants.dart
@@ -0,0 +1,4 @@
+/// Class containing all the image assets used in the application
+class ImageConstants {
+  ImageConstants._();
+}
--- lib/feature/login/presentation/login_screen.dart
+++ lib/feature/login/presentation/login_screen.dart
@@ -8,8 +8,19 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _loginEmailController = TextEditingController();
+  final TextEditingController _loginPasswordController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  @override
+  void dispose() {
+    _loginEmailController.dispose();
+    _loginPasswordController.dispose();
+    super.dispose();
   }
 }
